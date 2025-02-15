d57407bdefdb6f5be044437a44957156bd18b080
Karthik Ramadugu <119596201+karthiksai109@users.noreply.github.com>
2025-02-15 15:38:19 -0500

    Update userController.js

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt.js");
 const { default: mongoose } = require("mongoose");
 
 const userModel = require("../Models/userModel");
